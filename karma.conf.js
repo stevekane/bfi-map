aef984572858b55ea6517c9ce423a2052f45985e
steve kane <skane@4-south.com>
2013-06-16 22:24:03 -0500

    replaced underscore with lodash (interoperable).  tests still pass etc

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ files = [
   {pattern: 'public/javascripts/libs/chai.js', include: true},
   {pattern: 'public/javascripts/libs/minispade.js', include: true},
   {pattern: 'public/javascripts/libs/stats.min.js', include: true},
-  {pattern: 'public/javascripts/libs/underscore.js', include: true},
+  {pattern: 'public/javascripts/libs/lodash.js', include: true},
   {pattern: 'public/javascripts/libs/Bacon.min.js', include: true},
   {pattern: 'public/dist/client.js', include: true},
   {pattern: 'tests/test-classes.js', include: true},
